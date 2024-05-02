3cb6c7b1727496623c83eb8c4e6100191407619f
Lee-HyunSoo <cris_lx@naver.com>
2024-05-02 14:28:29 +0900

    baekjoon

---
+++ src/baekjoon/Q1427.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.*;
+
+public class Q1427 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        String num = String.valueOf(n);
+
+        List<Integer> result = new ArrayList<>();
+        for (char c : num.toCharArray()) {
+            result.add(Character.getNumericValue(c));
+        }
+        Collections.sort(result, Collections.reverseOrder());
+
+        for (int i : result) {
+            System.out.print(i);
+        }
+    }
+}
