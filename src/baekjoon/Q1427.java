e3081e6d40fac36d5d7e0b29abc4d712bd6e9e74
Lee-HyunSoo <cris_lx@naver.com>
2024-05-02 14:39:37 +0900

    baekjoon

--- src/baekjoon/Q1427.java
+++ src/baekjoon/Q1427.java
@@ -8,17 +8,20 @@ public class Q1427 {
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
-        String num = String.valueOf(n);
+        char[] num = String.valueOf(n).toCharArray();
+        Arrays.sort(num);
+        String s = new StringBuilder(String.valueOf(num)).reverse().toString();
+        System.out.println(s);
 
-        List<Integer> result = new ArrayList<>();
-        for (char c : num.toCharArray()) {
-            result.add(Character.getNumericValue(c));
-        }
-//        Collections.sort(result, Collections.reverseOrder());
-        Collections.sort(result, (a, b) -> b - a);
-
-        for (int i : result) {
-            System.out.print(i);
-        }
+//        String num = String.valueOf(n);
+//        List<Integer> result = new ArrayList<>();
+//        for (char c : num.toCharArray()) {
+//            result.add(Character.getNumericValue(c));
+//        }
+////        Collections.sort(result, Collections.reverseOrder());
+//        Collections.sort(result, (a, b) -> b - a);
+//        for (int i : result) {
+//            System.out.print(i);
+//        }
     }
 }
